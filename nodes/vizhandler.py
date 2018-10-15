9038e9f2e878955ece89fe5c409bb521e0523379
Mathias Hauan Arbo <mathias.arbo@itk.ntnu.no>
2018-10-15 11:26:49 +0200

    Initial commit

---
+++ nodes/vizhandler.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import rospy
+import visualization_msgs.msg as viz_msg
+import py_viz_marker.srv as srvs
+import threading
+
+# Using labels this way instead of IDs is not optimal
+# but it works for my purposes.
+
+
+class VisualizationHandler(object):
+    def __init__(self, namespace="", rate=50):
+        rospy.init_node("vizhandler", anonymous=True)
+        self.namespace = namespace
+        self.lifetime = rospy.Time.from_sec(1.0/rate)
+        self.rate = rospy.Rate(rate)
+        self.lock = threading.Lock()
+        self.markers = {}
+
+        self.services = [
+            rospy.Service(namespace + "/add_marker",
+                          srvs.AddMarker,
+                          self.add_marker),
+            rospy.Service(namespace + "/remove_marker",
+                          srvs.RemoveMarker,
+                          self.remove_marker)
+        ]
+        self.publisher = rospy.Publisher("visualization_marker_array",
+                                         viz_msg.MarkerArray,
+                                         queue_size=1)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.publish()
+            self.rate.sleep()
+
+    def publish(self):
+        mrks = []
+        mrk_idx = 0
+        with self.lock:
+            for mrk_lbl, mrk_msg in self.markers.items():
+                mrk_msg.header.stamp = rospy.Time.now()
+                mrk_msg.id = mrk_idx
+                mrks.append(mrk_msg)
+                mrk_idx += 1
+            self.publisher.publish(mrks)
+
+    def add_marker(self, req):
+        with self.lock:
+            req.marker.lifetime = self.lifetime
+            self.markers[req.label] = req.marker
+        return srvs.AddMarkerResponse()
+
+    def remove_marker(self, req):
+        with self.lock:
+            del self.markers[req.label]
+        return srvs.RemoveMarkerResponse()
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Node to publish "
+                                     + "viz_markers, uses services.")
+    parser.add_argument("-namespace",
+                        help="Namespace to publish in.",
+                        type=str,
+                        default="")
+    parser.add_argument("-rate",
+                        help="Update rate and inverse lifetime.",
+                        type=int,
+                        default=50)
+    args, unknown_args = parser.parse_known_args()
+    viz_handler = VisualizationHandler(args.namespace, args.rate)
+    try:
+        viz_handler.run()
+    except rospy.ROSInterruptException:
+        pass
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Simple RVIZ markers through services
+This `rospy` node continuously publishes rviz markers at a particular rate (default=50).
+The namespace parameter (default="") specifies the namespace underwhich the `add_marker` and `remove_marker` services are to be placed.
+
+## Example usage
+With rviz up and running and visualizing MarkerArray at `/visualization_marker_array`:
+
+1. `rosrun py_viz_marker vizhandler.py -namespace my_viz -rate 50`
+
+2.`rosservice call /add_marker "label: 'asd'
+marker:
+  header:
+    frame_id: 'map'
+  id: 0
+  type: 1
+  action: 0
+  scale: {x: 2.0, y: 1.0, z: 1.0}
+  color: {r: 1.0, g: 0.0, b: 1.0, a: 1.0}
+"`
+
+## Note
+Internally we use the labels as ID's for the markers. This means that you can overwrite a marker by setting the same label, or remove it by referencing the label.
